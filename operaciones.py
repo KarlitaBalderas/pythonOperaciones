9459cfc341eea87256e255472f9e37fb8f25637f
Karla Gabriela Sandoval Balderas <166642284+KarlitaBalderas@users.noreply.github.com>
2025-11-26 11:57:19 -0600

    Add files via upload

---
+++ operaciones.py
@@ -0,0 +1,28 @@
+# Funcion que recibe una lista y regresa la sumatoria
+def sumar(numeros:list[float]) -> float:
+    res=sum(numeros)
+    return res
+
+#Funcion que recibe una lista y regresa su promedio
+def promediar(numeros:list[float]) ->float:
+    if( len(numeros)==0):
+        return 0
+    prom=sum(numeros) / len(numeros)
+    return prom
+
+#Contar las organizaciones en el archivo CSV
+import pandas as pd
+def contar_org(ruta):
+    datos=pd.read_csv(ruta, encoding="latin-1")
+    conteo= len(datos)
+    return conteo
+
+datos=[10, 20, 1.5, 70.2]
+s = sumar(datos)
+print("Sumatoria ", s)
+
+p=promediar(datos)
+print("Promedio ", p)
+
+cant=contar_org("Organization_v3.csv")
+print("Cantidad de organizaciones ",cant)
